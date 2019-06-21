ac86c35cc71d3f8b7e5bcfd5da95daa989a501c2
Nick W <nicholasrwhaley@gmail.com>
2019-06-20 20:34:35 -0700

    add-flashing-effect

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
 let playerScore = 0
 let best = 0;
 let sequenceLength = 3;
-let turn = 0;
 let sequence = []
+let turn = 0;
 
 
 
@@ -23,9 +23,40 @@ let solution = document.getElementById('cheat')
 let feedback= document.getElementById('reaction')
 
 
+//Making the buttons flash
+let buttonOneFlash = function(){
+    buttonOne.style.backgroundColor = 'lightblue'
+ 
+     setTimeout(function () {
+             buttonOne.style.backgroundColor = 'blue'
+     }, 500)
+ }
+ 
+ let buttonTwoFlash = function(){
+    buttonTwo.style.backgroundColor = 'rgb(243, 188, 85)'
+ 
+     setTimeout(function () {
+             buttonTwo.style.backgroundColor = 'orange'
+     }, 500)
+ }
+ 
+ let buttonThreeFlash = function(){
+    buttonThree.style.backgroundColor = 'orangered'
+ 
+     setTimeout(function () {
+             buttonThree.style.backgroundColor = 'red'
+     }, 500)
+ }
+ 
+ let buttonFourFlash = function(){
+    buttonFour.style.backgroundColor = 'lightgreen'
+ 
+     setTimeout(function () {
+             buttonFour.style.backgroundColor = 'green'
+     }, 500)
+ }
 
-
-
+ //starting the Game
 let startTheGame = function(){
     turn = 0;
     solution.textContent = " "
@@ -34,11 +65,12 @@ let startTheGame = function(){
       let boxNumber = (Math.floor(Math.random()*4)+1)
       sequence.push(boxNumber)
       solution.textContent += boxNumber
-
   }
+  
+     
 }
 
-
+//feedback to player
 let getCompliment = function(){
     let num = (Math.floor(Math.random()*4)+1)
     switch(num){
@@ -54,11 +86,11 @@ let getCompliment = function(){
 }
 
 
-
-let repeatTheGame = function(){
-
+//replay button
+function repeatTheGame(sequence){
+     
 }
-
+//reset button
 let resetTheGame = function(){
     if(playerScore > best){
         highScore.textContent= playerScore;
@@ -72,20 +104,25 @@ let resetTheGame = function(){
     turn = 0
 }
 
-
+//effects of clicking button
 let clickButtonOne = function() {
+    buttonOneFlash();
     if(sequence[turn] === 1){
         feedback.textContent = getCompliment();
         playerScore++
         totalScore.textContent = playerScore;
           
-    } else{
+    } 
+    
+    else{
         feedback.textContent = "Oops, try again!"
     }
+   
     turn++
 }
 
 let clickButtonTwo = function() {
+    buttonTwoFlash()
     if(sequence[turn] === 2){
         feedback.textContent = getCompliment()
         playerScore++
@@ -98,6 +135,7 @@ let clickButtonTwo = function() {
 }
 
 let clickButtonThree = function() {
+    buttonThreeFlash()
     if(sequence[turn] === 3){
         feedback.textContent = getCompliment()
         playerScore++
@@ -111,6 +149,7 @@ let clickButtonThree = function() {
 }
 
 let clickButtonFour = function() {
+    buttonFourFlash();
     if(sequence[turn] === 4){
         feedback.textContent = getCompliment()
         playerScore++
@@ -128,11 +167,12 @@ let clickButtonFour = function() {
 
 
 
-
+//event listeners for buttons
 buttonOne.addEventListener('click', clickButtonOne)
 buttonTwo.addEventListener('click', clickButtonTwo)
 buttonThree.addEventListener('click', clickButtonThree)
 buttonFour.addEventListener('click', clickButtonFour)
 resetButton.addEventListener('click', resetTheGame)
 playButton.addEventListener('click', startTheGame)
+
 
